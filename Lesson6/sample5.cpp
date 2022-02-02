1838b2ce26a31b7a51e20ac0d6c96fa386d9e558
ruka charo <ruka.and.little.charo1@gmail.com>
2022-02-02 23:06:24 +0900

    Lesson6_practice2

---
+++ Lesson6/sample5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 1;
+
+  while(i <= 5){
+    cout << i << "回目の繰り返しです。\n";
+    i++;
+  }
+
+  cout << "繰り返しが終わりました。\n";
+
+  return 0;
+}
---
+++ Lesson6/sample7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 1;
+
+  do{
+    cout << i << "回目の繰り返しです。\n";
+    i++;
+  }while(i <= 5);
+
+  cout << "繰り返しが終わりました。\n";
+
+  return 0;
+}
